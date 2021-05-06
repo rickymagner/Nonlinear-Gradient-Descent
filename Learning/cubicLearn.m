f6236a01ef0df5bf8fbdf52055e4519d59932292
rickymagner <81349869+rickymagner@users.noreply.github.com>
2021-05-06 17:51:04 -0400

    Changed things around to focus on cubic reg.

--- Learning/cubicLearn.m
+++ Learning/cubicLearn.m
@@ -27,7 +27,16 @@ end
 % Plot results
 t = linspace(-2, 2, 100);
 pred = Theta(1) + Theta(2)*t + Theta(3)*t.^2 + Theta(4)*t.^3;
-plot(t, pred)
+plot(t, pred, 'r')
 hold on;
-scatter(X(:,2), y)
+color = [31/256, 119/256, 180/256];
+color_mat = reshape(repmat(color, 1, length(X(:,2))), 3, length(X(:,2)))';
+sz = ones(length(X(:,2)),1)*25;
+scatter(X(:,2), y, sz, color_mat, "filled")
+
+name = title("Cubic regression curve");
+leg = legend(strcat("Curve: y =  " , num2str(Theta(4)) , "x^3 " , num2str(Theta(3)) , "x^2 " , num2str(Theta(2)) , "x + " , num2str(Theta(1))));
+set(name, "fontsize", 20);
+set(leg, "fontsize", 16);
+
 %plot(J_list)
